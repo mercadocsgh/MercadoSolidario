0aaff6185a9a0d0806e18fdebb4a4f229f1392fd
vitor <mercadocs2024@gmail.com>
2026-03-01 15:01:14 -0300

    adiconado o soatório de kg por item e total no relatório de consumo por período

--- Mercado/views.py
+++ Mercado/views.py
@@ -792,7 +792,7 @@ def relatoriosConsumoPeriodo(request):
     tot_kg = 0
     for item in itensAtendimentos:
       produto = ProdutoSolidario.objects.filter(id__exact=item['id_codigo_id']).first()
-      item['kg'] = ceil(convert_tokg(produto.unidade, produto.quantidade)) * item['tot_itens']
+      item['kg'] = ceil(convert_tokg(produto.unidade, produto.quantidade) * item['tot_itens'])
       tot_kg += item['kg']
 
     context = {
